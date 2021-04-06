cafe2ed1f5d962321912cf0787b6fc1162afa1ba
Lucas Rodrigues <luckypretto@gmail.com>
2021-04-06 14:38:14 -0300

    Push

---
+++ ex008.py
@@ -0,0 +1,9 @@
+# Exercício Python 008: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+dis = float(input('Uma distância em metros: '))
+print(f'A medida de {dis}m corresponde a')
+print(str(dis/1000) + 'km')
+print(str(dis/100) + 'hm')
+print(str(dis/10) + 'dam')
+print('{:.0f}dm'.format(dis*10))
+print('{:.0f}cm'.format(dis*100))
+print('{:.0f}mm'.format(dis*100))
---
+++ ex009.py
@@ -0,0 +1,9 @@
+# Exercício Python 009: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+n = int(input('Digite um número para ver sua tabuada: '))
+print('-'*12)
+for c in range(1, 11):
+    if c != 10:
+        print(f'{n} x  {c} = {n*c}')
+    else:
+        print(f'{n} x {c} = {n*c}')
+print('-'*12)
